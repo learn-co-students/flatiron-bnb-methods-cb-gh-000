5e4e2ca3221351a8d76de6140c4bb6a4fafe5f9e
Robert Wayne <robertjwayne@gmail.com>
2017-05-26 17:54:50 -0400

    Add validation to ensure that the guest cannot be the host

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,15 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validate :guest_cannot_be_host
   validates :checkin, presence: true
   validates :checkout, presence: true
+
+  private
+
+  def guest_cannot_be_host
+    if self.listing.host == self.guest
+      errors.add(:listing, "Cannot create a reservation if guest is the host for a listing.")
+    end
+  end
 end
