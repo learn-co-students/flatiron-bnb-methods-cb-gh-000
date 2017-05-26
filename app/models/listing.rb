ed469839ca8ef35988c3cdd6b870301f045d8de1
Robert Wayne <robertjwayne@gmail.com>
2017-05-26 17:08:49 -0400

    Add set_host method to run before creating a listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,10 +5,18 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
 
+  before_create :set_host
+
   validates :address, presence: true
   validates :listing_type, presence: true
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :neighborhood_id, presence: true
+
+  private
+
+  def set_host
+    self.host.update(host: true)
+  end
 end
