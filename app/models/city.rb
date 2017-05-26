9c4023ee059854fe657dd264380d1045a378d517
Robert Wayne <robertjwayne@gmail.com>
2017-05-26 15:53:46 -0400

    Add #periods_overlap and refactor #city_openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,28 +3,27 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(start_date, end_date)
-    listings = self.listings
-
-    open_listings = listings.collect do |listing|
-      reservations = listing.reservations
-
-      overlapping_reservations = reservations.collect do |reservation|
-        reservation_checkin = reservation.checkin.to_s
-        reservation_checkout = reservation.checkout.to_s
-
-        if reservation_checkin <= end_date && reservation_checkout >= start_date
-          reservation_overlaps_user_dates = true
-        else
-          reservation_overlaps_user_dates = false
-        end
-
-        reservation if reservation_overlaps_user_dates
+    open_listings = self.listings.collect do |listing|
+      overlapping_reservations = listing.reservations.collect do |reservation|
+        reservation if periods_overlap?(reservation.checkin.to_s,
+                                        reservation.checkout.to_s,
+                                        start_date,
+                                        end_date)
       end
-
       listing if overlapping_reservations.compact.empty?
     end
 
     open_listings.compact
+  end
+
+  private
+
+  def periods_overlap?(period_one_start, period_one_end, period_two_start, period_two_end)
+    if period_one_start <= period_two_end && period_one_end >= period_two_start
+      true
+    else
+      false
+    end
   end
 end
 
