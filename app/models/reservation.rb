27d9b395b01be49f1d67879c883699e32a8e28e4
Robert Wayne <robertjwayne@gmail.com>
2017-05-29 12:26:23 -0400

    Add validations for availability of checkin and checkout dates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,11 +7,26 @@ class Reservation < ActiveRecord::Base
   validates :checkin, presence: true
   validates :checkout, presence: true
 
+  before_validation :checkin_date_must_be_available
+  before_validation :checkout_date_must_be_available
+
   private
 
   def guest_cannot_be_host
     if self.listing.host == self.guest
       errors.add(:listing, "Cannot create a reservation if guest is the host for a listing.")
+    end
+  end
+
+  def checkin_date_must_be_available
+    unless self.checkin && self.listing.available_for_res?(self.checkin)
+      errors.add(:checkin, "The check-in date is not available.")
+    end
+  end
+
+  def checkout_date_must_be_available
+    unless self.checkout && self.listing.available_for_res?(self.checkout)
+      errors.add(:checkout, "The check-out date is not available.")
     end
   end
 end
