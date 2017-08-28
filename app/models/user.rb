d1ea20d79d990e29d5d0b36be482dfc045c16351
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-08-28 07:50:06 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
+  has_many :guests, :through => :reservations
+  has_many :host_reviews, :through => :guests, :source => "reviews"
+
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+  has_many :accomodations, :through => :trips, :source => "listing"
+  has_many :hosts, :through => :accomodations
 end
