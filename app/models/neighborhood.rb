adfdd38ef600441adbac95bb8e4a257637727bcb
Carlos Rivas <carlosr1765@gmail.com>
2017-11-16 18:53:28 +0000

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,41 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  def neighborhood_openings(from, to)
+    openings = []
 
+    self.listings.each do |listing|
+      if listing.reservations.none? {|r|  r.checkin <= to.to_date and r.checkout >= from.to_date }
+        openings << listing
+      end
+
+    end
+
+    openings
+  end
+
+  def self.highest_ratio_res_to_listings
+    record = {highest_reservations: 0, neighbor_with_highest_r: nil, temp: 0}
+
+    Neighborhood.all.each do |neighbor|
+      record[:temp] = 0
+
+      neighbor.listings.each do |listing|
+        record[:temp] = record[:temp] + listing.reservations.count
+      end
+
+      if record[:highest_reservations] < record[:temp]
+        record[:highest_reservations] = record[:temp]
+        record[:neighbor_with_highest_r] = neighbor
+      end
+
+    end
+
+    record[:neighbor_with_highest_r]
+  end
+
+
+  def self.most_res
+    self.highest_ratio_res_to_listings
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,17 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates :description, presence: true
+  validates :rating, presence: true
+  validates :reservation, presence: true
+  validate :reservation_happened
+
+  def reservation_happened
+    if !!reservation
+      if self.reservation.checkout > Time.now
+        errors.add(:reservation_happened, 'Must have checkout before adding a review')
+      end
+    end
+  end
 
 end
