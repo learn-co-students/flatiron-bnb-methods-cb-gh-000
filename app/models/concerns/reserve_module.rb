febf5bf69e0daee2c91641784ec031cd9cd7a190
Rauof M <abdelrauof197@gmail.com>
2018-03-09 13:11:48 +0000

    Done

---
+++ app/models/concerns/reserve_module.rb
@@ -0,0 +1,20 @@
+
+class ValidateGuestReservation < ActiveModel::Validator
+  def validate(record)
+    if record.guest == record.listing.host
+      record.errors[:host] << 'Need a name starting with X please!'
+    end
+  end
+end
+
+class ListingCheckinAvailability < ActiveModel::Validator
+  def validate(record)
+    checkin_dates = []
+    record.listing.reservation.each do |reservation|
+      checkin_dates << reservation.checkin
+    end
+    unless checkin_dates.include?(record.checkin)
+      record.errors[:checkin] << 'checkin not available'
+    end
+  end
+end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,16 @@
+require_relative './concerns/reserve_module.rb'
+
 class Reservation < ActiveRecord::Base
+  
+  include ActiveModel::Validations
+  
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
-
+  
+  validates :checkin, presence: true 
+  validates :checkout, presence: true 
+  validates_with ValidateGuestReservation
+  validates_with ListingCheckinAvailability
+  
 end
