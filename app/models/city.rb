29215748783253e464d087dea15eae64604fb02a
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-04-28 05:58:05 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,28 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+   has_many :reservations, :through => :listings
 
+  def city_openings(start_date, end_date)
+      listings.merge(Listing.available(start_date, end_date))
+  end
+
+  def ratio_reservations_to_listings
+    if listings.count > 0
+      reservations.count.to_f / listings.count.to_f
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+
+    all.max do |a, b|
+      a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
+    end
+  end
+
+  def self.most_res
+    all.max do |a, b|
+      a.reservations.count <=> b.reservations.count
+    end
+  end
 end
-
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,40 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :listing_type, presence: true
+  validates :neighborhood, presence: true
+
+  after_save :set_host
+  before_destroy :unset_host
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  def set_host
+    host.update(host: true)
+  end
+
+  def unset_host
+    if Listing.where(host: host).where.not(id: id).empty?
+     host.update(host: false)
+   end
+  end
+
+
+  def self.available(start_date, end_date)
+    if start_date && end_date
+      joins(:reservations).
+        where.not(reservations: {checkin: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {checkout: start_date..end_date})
+    else
+      []
+    end
+  end
 end
